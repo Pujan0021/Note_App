32d9e314a77b4cd01a5c4fca96db0600ddfdc18a
Pujan-Pandey <pujanpandey21@gmail.com>
2026-01-12 18:50:07 +0545

    DB Connection

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+
 const connectToMongoDB = async () => {
+    const CONN_STR = process.env.CONN_STR;
     try {
-        await mongoose.connect("mongodb+srv://pujanpandey0071_db_user:7groxOVZy6Vv039O@chatloop.ivrozy4.mongodb.net/?appName=Note_App");
+        if (!CONN_STR) {
+            console.log("CONN_STR is not found")
+        }
+
+        await mongoose.connect(CONN_STR);
         console.log("DataBase Connected SuccessFully");
     } catch (err) {
         console.log("Error Occured connecting Database", err);
+        process.exit(1);
     }
 }
-//Mongo_URI = "mongodb+srv://pujanpandey0071_db_user:7groxOVZy6Vv039O@chatloop.ivrozy4.mongodb.net/?appName=ChatLoop"
 
 module.exports = connectToMongoDB;
