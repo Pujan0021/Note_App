91004e4d9ad2db5c6feb7e8047dc41a2ac7f6c14
Pujan-Pandey <pujanpandey21@gmail.com>
2026-01-13 18:47:37 +0545

    Cookie Parser Added

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,18 @@
 const express = require("express");
+const cookieParser = require("cookie-parser")
 const PORT = 3000;
 const app = express();
 const cors = require("cors");
 const router = require("./src/routes/auth.route");
+const noteRouter = require("./src/routes/note.route");
 const connectToMongoDB = require("./src/config/db");
 // const cors = require("cors");
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser())
 app.use("/api/auth", router);
+app.use("/api/note", noteRouter);
 // app.get("/", (req, res) => {
 //     res.send("--Note App--");
 // });
