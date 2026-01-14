9ce66f6dc37eff4db5186df3631c25787fc32d31
Pujan-Pandey <pujanpandey21@gmail.com>
2026-01-14 19:51:09 +0545

    Port

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cookieParser = require("cookie-parser")
-const PORT = 3000;
 const app = express();
 const cors = require("cors");
 const router = require("./src/routes/auth.route");
 const noteRouter = require("./src/routes/note.route");
 const connectToMongoDB = require("./src/config/db");
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
 // const cors = require("cors");
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
