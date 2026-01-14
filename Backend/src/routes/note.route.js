2b899bf0d9085d1701b50218c88ce324a9b1e464
Pujan-Pandey <pujanpandey21@gmail.com>
2026-01-14 11:03:59 +0545

    Update & Delete routes

--- Backend/src/routes/note.route.js
+++ Backend/src/routes/note.route.js
@@ -4,14 +4,10 @@ const authMiddleware = require("../middleware/auth.middleware");
 const router = express.Router();
 router.post("/add", authMiddleware, async (req, res) => {
     try {
-
         const { title, description } = req.body;
-
         if (!title || !description) {
             return res.status(400).json({ success: false, message: "Title and Description are required" });
         }
-
-
         const newNote = new Note({ title, description, userId: req.user.id });
         await newNote.save();
         return res.status(200).json({
@@ -26,6 +22,30 @@ router.post("/add", authMiddleware, async (req, res) => {
         return res.status(500).json({ success: false, message: "Error adding a note!" });
     }
 })
-
-
+router.get("/notes", authMiddleware, async (req, res) => {
+    try {
+        const notes = await Note.find({ userId: req.user.id });
+        return res.status(200).json({ success: true, notes })
+    } catch (error) {
+        return res.status(500).json({ success: false, messsage: "Cannot Retrieve Note" })
+    }
+})
+router.put("/update/:id", authMiddleware, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updateNote = await Note.findByIdAndUpdate(id, req.body);
+        return res.status(201).json({ success: true, updateNote })
+    } catch (error) {
+        return res.status(500).json({ success: false, messsage: "Cannot Update Note" })
+    }
+})
+router.delete("/delete/:id", authMiddleware, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updateNote = await Note.findByIdAndDelete(id, req.body);
+        return res.status(201).json({ success: true, message: "Note deleted successfully" })
+    } catch (error) {
+        return res.status(500).json({ success: false, messsage: "Cannot Update Note" })
+    }
+})
 module.exports = router;
