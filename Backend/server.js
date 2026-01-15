18fc5a99aa1bc9191cef47a60735ee2b16dc5a23
Pujan-Pandey <pujanpandey21@gmail.com>
2026-01-15 19:58:29 +0545

    Cors

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,17 @@
 const express = require("express");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const router = require("./src/routes/auth.route");
 const noteRouter = require("./src/routes/note.route");
 const connectToMongoDB = require("./src/config/db");
 require("dotenv").config();
+
 const PORT = process.env.PORT || 3000;
-import cors from "cors";
-import express from "express";
 const app = express();
 
-
 const allowedOrigins = [
-    "http://localhost:5173", // local dev
-    "https://note-app-frontend-vewt.onrender.com" // deployed frontend
+    "http://localhost:5173",
+    "https://note-app-frontend-vewt.onrender.com"
 ];
 
 app.use(cors({
@@ -24,18 +22,17 @@ app.use(cors({
             callback(new Error("Not allowed by CORS"));
         }
     },
-    credentials: true // if you’re using cookies/JWT in headers
+    credentials: true
 }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
+
 app.use("/api/auth", router);
 app.use("/api/note", noteRouter);
-// app.get("/", (req, res) => {
-//     res.send("--Note App--");
-// });
+
 app.listen(PORT, () => {
     connectToMongoDB();
-    console.log("---------   Server Started   ------------ ")
-})
+    console.log("---------   Server Started   ------------ ");
+});
