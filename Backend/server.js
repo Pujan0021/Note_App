c67c3b991dbd0e847f06a2539c012227eef97ae8
Pujan-Pandey <pujanpandey21@gmail.com>
2026-01-15 19:53:18 +0545

    Front Url Added

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const cookieParser = require("cookie-parser")
-const app = express();
 const cors = require("cors");
 const router = require("./src/routes/auth.route");
 const noteRouter = require("./src/routes/note.route");
 const connectToMongoDB = require("./src/config/db");
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
+import cors from "cors";
+import express from "express";
+const app = express();
+
+
+const allowedOrigins = [
+    "http://localhost:5173", // local dev
+    "https://note-app-frontend-vewt.onrender.com" // deployed frontend
+];
+
 app.use(cors({
-    origin: "https://note-app-frontend-vewt.onrender.com", // <-- frontend domain
-    credentials: true
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true // if you’re using cookies/JWT in headers
 }));
 
 app.use(express.json());
